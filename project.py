54e6eb271fe23c6109dbffb93f1a7e520644e627
nianentang <124058516+nianentang@users.noreply.github.com>
2023-05-02 08:17:32 -0400

    Update project.py

--- project.py
+++ project.py
@@ -21,14 +21,18 @@ class Water:
         self.moves
         moves = water_abilities["name"]
 
+random = random.randint(0,4)
 ability = input("What ability do you want? WATER/FIRE")
+
 if ability.upper() == "WATER":
     watermove = input("Choose a water move")
-    random = random.randint(0,4)
-    while random > 0:
+    if random == 0:
+        print("Your move was inffective")
+    elif random > 0:   
         print("Your move was effective")
         
 if ability.upper() == "FIRE":
-    random = random.randint(0,4)
-    while random > 0:
+    if random == 0:
+        print("Your move was inffective")
+    elif random > 0:
         print("Your move was effective")
