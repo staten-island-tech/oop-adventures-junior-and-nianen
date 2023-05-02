dec51164fe96edffc2849d9387e8412d9cd1394e
nianentang <124058516+nianentang@users.noreply.github.com>
2023-05-02 08:38:20 -0400

    Update project.py

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
-import fire_abilities
-import water_abilities
 import random
 
+import fire_abilities
+import water_abilities
 
 name = input("What is your name?")
 class User:
@@ -12,27 +12,45 @@ class User:
 class Fire:
     def __init__(self, id, name, moves):
         super().__init__(id,name)
-        self.moves
+        self.moves = moves
         moves = fire_abilities["name"]
 
 class Water:
     def __init__(self, id, name, moves):
         super().__init__(id,name)
-        self.moves
+        self.moves = moves
         moves = water_abilities["name"]
+class Enemie: 
+    def __init__(self, id, name, health):
+        super().__init__(id,name)
+        self.health = health
+class Boss:
+     def __init__(self, id, name, health):
+        super().__init__(id,name)
+        self.health = health
 
+random1 = random.randint(0,4)
 random = random.randint(0,4)
+
 ability = input("What ability do you want? WATER/FIRE")
 
+
+
 if ability.upper() == "WATER":
-    watermove = input("Choose a water move")
-    if random == 0:
-        print("Your move was inffective")
-    elif random > 0:   
+    watermove = input("Choose a water move" )
+    if random > 0:
         print("Your move was effective")
+    elif random == 0:
+        print("Your move was ineffective")
+
         
 if ability.upper() == "FIRE":
-    if random == 0:
-        print("Your move was inffective")
-    elif random > 0:
+    if random > 0:
         print("Your move was effective")
+    elif random == 0:
+        print("Your move was ineffective")
+    
+        
+
+
+
