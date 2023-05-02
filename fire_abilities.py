e16509049240b5e0b54d0da7b5cb20fc0b9d7b83
juniorz5 <juniorz5@nycstudents.net>
2023-05-02 08:38:12 -0400

    Update fire_abilities.py

--- fire_abilities.py
+++ fire_abilities.py
@@ -1,25 +1,30 @@
 [
-    {
-"id":1,
-"name": {
-"Fire Fist"
-    }
-'type: 
-{
-"Burning",
-"Bleeding"
-},
-"base" : {
-"Attack": 50 
-    },
-{
-"id":2
-"name":{
-"Fire Blast"
-}
-}
-    }
+            {"id": 1,
+            "name": {
+                "english": "Fire Fist"
+            }
+            "type": [
+                "Burning",
+                "Bleeding"
+            ],
+            "base" : {
+            "Attack": 50 
+            },
+        },
+        {
+            "id":2,
+            "name":{
+                "english": "Fire Blast"
+            },
+            "type":[
+                "Burning"
+            ],
+            "base": {
+            "Attack":40
+            }
+        } 
 ]
+
 
 
 
