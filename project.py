3f2098e16a3dec060c31f9ba10bf62686d725f80
nianentang <124058516+nianentang@users.noreply.github.com>
2023-05-03 08:38:37 -0400

    project

--- project.py
+++ project.py
@@ -5,31 +5,40 @@ import water_abilities
 
 name = input("What is your name?")
 class User:
-    def __init__ (self, id, name):
+    def __init__ (self, id, name, health, max_health):
         self.id = id
         self.name = name
+        self.health = health
+        self.max_health = max_health
 
 class Fire:
-    def __init__(self, id, name, moves):
-        super().__init__(id,name)
-        self.moves = moves
-        moves = fire_abilities["name"]
-
-class Water:
-    def __init__(self, id, name, moves):
-        super().__init__(id,name)
+    def __init__(self, id, name, moves, health, max_health):
+        super().__init__(id, name, health, max_health)
         self.moves = moves
         moves = water_abilities["name"]
-class Enemie: 
-    def __init__(self, id, name, health):
-        super().__init__(id,name)
-        self.health = health
-class Boss:
-     def __init__(self, id, name, health):
-        super().__init__(id,name)
-        self.health = health
 
-random1 = random.randint(0,4)
+class Water:
+    def __init__(self, id, name, moves, health, max_health):
+        super().__init__(id,name, health, max_health)
+        self.moves = moves
+        moves = fire_abilities["name"]
+       
+class Enemy: 
+    def __init__(self, id, name, health, max_health):
+        self.health = health
+        self.id = id
+        self.name = name
+        self.max_health = max_health
+
+class Boss:
+     def __init__(self, id, name, health, max_health):
+        self.health = health
+        self.id = id
+        self.name = name
+        self.max_health = max_health
+
+
+
 random = random.randint(0,4)
 
 ability = input("What ability do you want? WATER/FIRE")
@@ -38,17 +47,20 @@ ability = input("What ability do you want? WATER/FIRE")
 
 if ability.upper() == "WATER":
     watermove = input("Choose a water move" )
-    if random > 0:
-        print("Your move was effective")
-    elif random == 0:
-        print("Your move was ineffective")
+    if watermove == water_abilities["name"]:
+        if random > 0:
+            print("Your move was effective")
+        elif random == 0:
+            print("Your move was ineffective")
 
         
 if ability.upper() == "FIRE":
-    if random > 0:
-        print("Your move was effective")
-    elif random == 0:
-        print("Your move was ineffective")
+    firemove = input("Choose a fire move" )
+    if firemove == fire_abilities["name"]:
+        if random > 0:
+            print("Your move was effective")
+        elif random == 0:
+            print("Your move was ineffective")
     
         
 
