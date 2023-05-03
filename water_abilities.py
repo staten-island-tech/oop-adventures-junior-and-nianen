0532bd5a74e5e172ff77db7813c59a5e8451d9d8
juniorz5 <juniorz5@nycstudents.net>
2023-05-03 08:38:18 -0400

    update

---
+++ water_abilities.py
@@ -0,0 +1,14 @@
+men_1 = [{
+    "id": 1,
+    "name":{
+        "english":"Ice Breath"
+    },
+    "type":[
+        "Freezing",
+        "AOA"
+    ],
+    "base":{
+        "Attack":"Base"
+    }
+}]
+print(men_1)
