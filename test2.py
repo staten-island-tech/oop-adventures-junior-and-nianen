4d3e312db76cc0bdae3aa78211d2082b27ad4137
juniorz5 <juniorz5@nycstudents.net>
2023-05-15 08:22:41 -0400

    update

--- test2.py
+++
@@ -1,6 +0,0 @@
-import json
-with open('water_abilities.json') as water_abilities_file:
-    file_contents = water_abilities_file.read()
-
-print ("Name:" + file_contents[150:200])
-
--- test3.py
+++
@@ -1,7 +0,0 @@
-my_dictionary = {"song": "Estranged", "artist": "Guns N' Roses"}
-print(my_dictionary["song"])
-
-import json
-with open('water_abilities.json') as water_abilities_file:
-    file_contents = water_abilities_file.read()
-print(file_contents["id"])
