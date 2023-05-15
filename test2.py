811c457d3d0e2838ec62dbca50a643ec9e4f6009
juniorz5 <juniorz5@nycstudents.net>
2023-05-15 08:04:34 -0400

    update

--- test2.py
+++ test2.py
@@ -2,5 +2,5 @@ import json
 with open('water_abilities.json') as water_abilities_file:
     file_contents = water_abilities_file.read()
 
-print (file_contents)
+print ("Name:" + file_contents[150:200])
 
