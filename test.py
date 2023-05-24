8cb4cfec1bb23e75f8a63427e38bded27a0cf0c5
juniorz5 <juniorz5@nycstudents.net>
2023-05-24 08:32:26 -0400

    update

--- test.py
+++ test.py
@@ -3,16 +3,19 @@ import json
 with open("water_abilities.json", encoding="utf8") as water_ability_file:
     water_abilities = json.load(water_ability_file)
 empty_list = []
-damage_dealty = (empty_list)
 watermove = input("Choose a water move (Ice Breath, Tsunami, Icecle): ")
 for abilities in water_abilities:
     if watermove == abilities["name"]["english"]:
-        print(abilities['base']['Attack']) and empty_list.append(abilities.copy(['base']['Attack']))
-
-print('You have encountered another enemy')
+        print(abilities['base']['Attack'])
+        for abilities in water_abilities:
+            if watermove == abilities["name"]["english"]:
+                empty_list.append(abilities['base']['Attack'])
 watermove = input("Choose a water move (Ice Breath, Tsunami, Icecle): ")
 for abilities in water_abilities:
     if watermove == abilities["name"]["english"]:
-        print(abilities['base']['Attack']) and empty_list.append(abilities.copy(['base']['Attack']))
-
-print(f'You Dealt A total Damage of'[damage_dealty])
+        print(abilities['base']['Attack'])
+        for abilities in water_abilities:
+            if watermove == abilities["name"]["english"]:
+                empty_list.append(abilities['base']['Attack'])
+damage_dealty = sum(empty_list)
+print(damage_dealty)
