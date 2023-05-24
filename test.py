4674d770403f089e32d37a5b2d306844df28c760
juniorz5 <juniorz5@nycstudents.net>
2023-05-24 08:38:59 -0400

    update

--- test.py
+++ test.py
@@ -1,21 +1,13 @@
 import json 
-
 with open("water_abilities.json", encoding="utf8") as water_ability_file:
     water_abilities = json.load(water_ability_file)
 empty_list = []
 watermove = input("Choose a water move (Ice Breath, Tsunami, Icecle): ")
-for abilities in water_abilities:
-    if watermove == abilities["name"]["english"]:
-        print(abilities['base']['Attack'])
-        for abilities in water_abilities:
-            if watermove == abilities["name"]["english"]:
-                empty_list.append(abilities['base']['Attack'])
-watermove = input("Choose a water move (Ice Breath, Tsunami, Icecle): ")
-for abilities in water_abilities:
-    if watermove == abilities["name"]["english"]:
-        print(abilities['base']['Attack'])
-        for abilities in water_abilities:
-            if watermove == abilities["name"]["english"]:
-                empty_list.append(abilities['base']['Attack'])
-damage_dealty = sum(empty_list)
-print(damage_dealty)
+
+def appening_function ():
+    for abilities in water_abilities:
+        if watermove == abilities["name"]["english"]:
+            print(abilities['base']['Attack'])
+            for abilities in water_abilities:
+                if watermove == abilities["name"]["english"]:
+                    empty_list.append(abilities['base']['Attack'])
